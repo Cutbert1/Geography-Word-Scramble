e23477ea85c7433729995465d85bda4c4c1e38eb
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2024-09-12 19:09:21 +0000

    Add functions to tally correct and incorrect answer with alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 // country Array
 
-const word = ["italy", "france", "china", "mexico", "india", "egypy", "peru", "brazil", 
+const country = ["japan", "italy", "france", "china", "mexico", "india", "egypt", "peru", "brazil", 
     "greece", "jordan", "germany", "united states of america", "turkey", "lebanon", "croatia", "tanzania", 
     "argentina", "cambodia", "australia", "spain", "untited arab emirate", "england", 
-    "thailand", "ecuador", "zambia", "nepal", "russia", "south africa"];
+    "thailand", "ecuador", "zambia", "nepal", "russia", "south africa", "portugal", "Singapore", 
+    "canada", "vietnam", "hong kong", "taiwan", "irland", "malaysia", "switzerland", "scotland", "indonesia", "belguim"];
 
 // hints list Array
-const hints = ["Colosseum", "Eiffel Tower", "Great Wall", "Chichen Itza", "Mont-Saint Michel", "Taj Mahal",
-     , "Pyramids of Giza", "Machu Picchu", "Christ the Redeemer", "Acropolis", "Petra", "The Brandenburg Gate", 
+const hints = ["Fushimi Inari Taisha", "Colosseum", "Eiffel Tower", "Great Wall", "Chichen Itza", "Taj Mahal",
+     "Pyramids of Giza", "Machu Picchu", "Christ the Redeemer", "Acropolis", "Petra", "The Brandenburg Gate", 
      "The Golden Gate Bridge", "The Blue Mosque", "Baalbek", "Plitvice Lakes National Park", "Kilimanjaro", "Perito Moreno Glacier", 
-     "Angkor Wat Temple Complex", "Great Barrier Reef", "Sagrada Familia", "Burj Khalifa", "Sydney Opera House", "Big Ben", "The Grand Palace", 
-     "The Galapagos Islands", "Serengeti National Park", "Victoria Falls", "Mount Everest", "Red Square", "Table Mountain"];
+     "Angkor Wat Temple Complex", "Great Barrier Reef", "Sagrada Familia", "Burj Khalifa", "Big Ben", "The Grand Palace", 
+     "The Galapagos Islands", "Victoria Falls", "Mount Everest", "Red Square", "Table Mountain", "Dom Luis Bridge", 
+     "Gardens by the Bay", "Niagara Falls" ,"Ha Long Bay", "Tian Tan Buddha", "Taipei 101", "Cliffs of Moher", 
+     "Petronas Twin Towers", "Chapel Bridge", "Edinburgh Castle", "Mount Bromo", "Atomium"];
 
 // Initialize display country with empty string
-let displayWord = " ";
+let displayCountry = " ";
 
 // Call function to shuffle letters
 function shuffle(str) {
@@ -36,19 +39,48 @@ function shuffle(str) {
 function check() {
     let input = document.getElementById("input");
     let declaration = document.getElementById("declaration");
-    if (input.value.toLocaleLowerCase() === displayWord.toLocaleLowerCase()
-    )
+
+    if (input.value.toLocaleLowerCase() === displayCountry.toLocaleLowerCase()) {
         declaration.innerHTML = "Result: Correct";
-    else declaration.innerHTML = "Result: Incorrect";
+        alert("Fantastic, You Got It Right!!")
+        incrementScore();
+
+    } else{
+        declaration.innerHTML = "Result: Incorrect";
+        alert(`Hmmm...You answered ${input.value}. The correct answer is ${displayCountry}`);
+        incrementWrongAnswer();
+    } 
+            
 }
+
+/**
+ * Get the current score from the DOM and increment it by 1
+ */
+function incrementScore() {
+
+    let initialScore = (document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++initialScore;
+
+}
+
+/**
+ * Get the current tally of incorrect answers from the DOM and increment it by 1
+ */
+function incrementWrongAnswer() {
+
+    let initialScore = (document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++initialScore;
+
+}
+
 
 //Refresh and show new record
 function refresh() {
-    index = Math.floor(Math.random() * 32); 
-    displayWord = word[index]; 
+    index = Math.floor(Math.random() * 41); 
+    displayCountry = country[index]; 
     displayHint = hints[index]; 
     scrambleWord = document.getElementById("scrambleWord");
-    scrambleWord.innerText = shuffle(displayWord).toUpperCase();
+    scrambleWord.innerText = shuffle(displayCountry).toUpperCase();
 
     let hint = document.getElementById("hint");
     hint.innerHTML = "<b>Hint</b>:" + displayHint;
