58b203b295be11194bf31eb5b165700bff0a582f
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2024-09-12 15:04:58 +0000

    Add const array of countries and hints

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// country Array
+
+const country = ["italy", "france", "china", "mexico", "india", "egypy", "peru", "brazil", 
+    "greece", "jordan", "germany", "usa", "turkey", "lebanon", "croatia", "tanzania", 
+    "argentina", "cambodia", "australia", "spain", "untited arab emirate", "england", 
+    "thailand", "ecuador", "zambia", "nepal", "russia", "south africa"];
+
+// hints list Array
+const hints = ["Trevi Fountain", "Eiffel Tower", "Great Wall", "Chichen Itza", "Mont-Saint Michel", "Taj Mahal",
+     "Colosseum", "Pyramids of Giza", "Machu Picchu", "Christ the Redeemer", "Acropolis", "Petra", "The Brandenburg Gate", 
+     "The Golden Gate Bridge", "The Blue Mosque", "Baalbek", "Plitvice Lakes National Park", "Kilimanjaro", "Perito Moreno Glacier", 
+     "Angkor Wat Temple Complex", "Great Barrier Reef", "Sagrada Familia", "Burj Khalifa", "Sydney Opera House", "Big Ben", "The Grand Palace", 
+     "The Galapagos Islands", "Serengeti National Park", "Victoria Falls", "Mount Everest", "Red Square", "Table Mountain"];
+
+// Initialize display country with empty string
+let displayCountry = " ";
